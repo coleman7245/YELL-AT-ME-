3c80a51c9089c1ed02d0fed59546e178ef7ddd87
smro <833stephanie@gmail.com>
2025-03-07 13:01:59 -0700

    adding post creation component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
+import PostCreation from './Components/PostCreation'
 import HomePage from './pages/HomePage'
 
 function App() {
   return (
-    <>
+    <div>
       {/* <HomePage></HomePage> */}
-    </>
+      <PostCreation username="true" comment="false"/>
+    </div>
+
+
+
+
   )
 }
 

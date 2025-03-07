4c872da0137fe97958c9a4f190c0460d4b6e3cf3
Fernando Babonoyaba <fernando.babonoyaba@gmail.com>
2025-03-07 11:53:06 -0700

    added home and data

---
+++ frontend/src/data.js
@@ -0,0 +1,62 @@
+let frustrationObjects = [
+    {
+        body: 'I CAN\'T BELIEVE THIS SYSTEM CRASHED AGAIN!',
+        user: 'MIKE!!',
+        comments: ['WHY DOES THIS ALWAYS HAPPEN?!', 'SO ANNOYING!', 'CAN\'T TAKE IT ANYMORE!!'],
+        exclamations: 3,
+    },
+    {
+        body: 'THIS CODE JUST WON\'T WORK NO MATTER WHAT!',
+        user: 'LUCY!!',
+        comments: ['WHY?!', 'I\'VE TRIED EVERYTHING!!', 'THIS IS IMPOSSIBLE!!'],
+        exclamations: 2,
+    },
+    {
+        body: 'I CAN\'T FIND MY KEYS ANYWHERE!!!',
+        user: 'TOM!!',
+        comments: ['I\'M GOING CRAZY!', 'I\'M LATE NOW!!', 'WHY DOES THIS ALWAYS HAPPEN TO ME?!'],
+        exclamations: 3,
+    },
+    {
+        body: 'WHY DOES THIS APP KEEP CRASHING?!',
+        user: 'SARAH!!',
+        comments: ['IT\'S SO FRUSTRATING!', 'THIS IS UNACCEPTABLE!!', 'I CAN\'T DEAL WITH THIS ANYMORE!!'],
+        exclamations: 3,
+    },
+    {
+        body: 'I CAN\'T GET THIS EMAIL TO SEND PROPERLY!',
+        user: 'JAMES!!',
+        comments: ['WHY?!', 'THIS IS A NIGHTMARE!', 'I\'M SO DONE WITH THIS!!'],
+        exclamations: 2,
+    },
+    {
+        body: 'EVERYTHING IS GOING WRONG TODAY!',
+        user: 'EMMA!!',
+        comments: ['I CAN\'T CATCH A BREAK!', 'THIS IS RIDICULOUS!!', 'SOMEBODY HELP ME!!'],
+        exclamations: 3,
+    },
+    {
+        body: 'THIS FILE IS TOO BIG TO UPLOAD!!',
+        user: 'CHRIS!!',
+        comments: ['WHY IS IT SO COMPLICATED?!', 'I\'VE TRIED EVERYTHING!', 'THIS IS GETTING OUT OF HAND!!'],
+        exclamations: 2,
+    },
+    {
+        body: 'I CAN\'T FIGURE OUT WHAT\'S WRONG WITH MY CAR!',
+        user: 'OLIVIA!!',
+        comments: ['THIS IS DRIVING ME CRAZY!', 'I\'VE BEEN TRYING TO FIX IT FOR HOURS!!', 'I CAN\'T TAKE THIS ANYMORE!!'],
+        exclamations: 3,
+    },
+    {
+        body: 'I\'VE BEEN WAITING FOR SO LONG AND NO ONE IS HELPING ME!',
+        user: 'LISA!!',
+        comments: ['THIS IS SO UNFAIR!', 'I\'VE HAD ENOUGH!!', 'I CAN\'T BELIEVE THIS IS HAPPENING!!'],
+        exclamations: 2,
+    },
+    {
+        body: 'I CAN\'T GET THIS STUPID PROGRAM TO RUN!',
+        user: 'RICHARD!!',
+        comments: ['I\'VE TRIED EVERYTHING!!!', 'THIS IS SO FRUSTRATING!!', 'WHY DOES THIS NEVER WORK?!'],
+        exclamations: 3,
+    }
+];
